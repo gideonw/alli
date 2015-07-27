14da5f2fdbbd65b3c97005ccf1376ce7ee52ad3c
Gideon Williams <gideon@gideonw.com>
2015-07-27 11:07:05 -0700

    Initial Commit

---
+++ alli/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  alli
+//
+//  Created by Gideon Williams on 7/27/15.
+//  Copyright (c) 2015 Gideon Williams. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
